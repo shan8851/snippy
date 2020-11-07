0d58cadbe466438ea026ddf98f1fe73bc4ea29fd
shan5742 <asamshan456@gmail.com>
2020-11-07 13:21:25 +0000

    removing env from next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withImages = require("next-images");
 module.exports = withImages({
   target: "serverless",
-  env: {
-    FAUNA_SECRET: process.env.FAUNA_SECRET,
-  },
   esModule: true,
   webpack(config, options) {
     return config;
