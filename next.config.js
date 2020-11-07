4d172f95ea6e0f52b133ba1e14ba6d1ce06ec5b0
shan5742 <asamshan456@gmail.com>
2020-11-07 13:02:12 +0000

    adding enc to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withImages = require("next-images");
 module.exports = withImages({
   target: "serverless",
+  env: {
+    FAUNA_SECRET: process.env.FAUNA_SECRET,
+  },
   esModule: true,
   webpack(config, options) {
     return config;
