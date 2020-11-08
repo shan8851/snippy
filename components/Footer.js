9459133a5c052a0acfac1ef5e3475f67d99801e2
shan5742 <asamshan456@gmail.com>
2020-11-08 10:07:52 +0000

    styling footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,10 @@ export default function Footer() {
   return (
     <div className="flex justify-center pt-8 pb-16">
       <p className="text-green-100 text-xl">
-        Made with ❤️ by <Link href="https://asamshan.dev/">Asam Shan</Link>
+        Made with ❤️ by{" "}
+        <Link href="https://asamshan.dev/">
+          <a className="font-bold hover:underline">Asam Shan</a>
+        </Link>
       </p>
     </div>
   );
--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="p-4 bg-green-300">
-      <Link href="/" className="font-bold text-2xl text-green-900">
-        Welcome to Snippy
+      <Link href="/">
+        <a className="font-bold text-2xl text-green-900">Welcome to Snippy</a>
       </Link>
     </div>
   );
